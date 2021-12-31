62c360dc6c08962043220461bea893e7e54d87c5
Lorenzo Murarotto <lnzmrr@gmail.com>
2021-12-31 14:16:53 +0000

    working implementation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,125 @@
-// const { Sudoku } = await import("../pkg/index");
+import { PlayResult, SolveResult, Sudoku } from "../pkg/index";
+import { memory } from "../pkg/index_bg.wasm";
+
+const MAX_SEED = 2 ** 32 - 1;
+const CLUE_QTY = 24;
+
+const timeouts = [];
+
+/**
+ * @param {HTMLElement} target
+ * @param {string} name
+ * @param {number} duration
+ */
+function addTransientClass(target, name, duration) {
+    const key = target.id + name;
+    if (timeouts[key]) clearTimeout(timeouts[key]);
+    target.classList.add(name);
+    const timeout = setTimeout(() => target.classList.remove(name), duration);
+    timeouts[key] = timeout;
+}
+
+const params = new URLSearchParams(window.location.search);
+
+function generateSeed() {
+    const seed = Math.floor(Math.random() * MAX_SEED);
+    params.set("seed", seed);
+    window.location.search = params;
+}
+
+if (!params.has("seed")) generateSeed();
+
+const seed = params.get("seed");
+const sudoku = Sudoku.random(seed, CLUE_QTY);
+const gridBuffer = new Uint8Array(memory.buffer, sudoku.getGrid(), sudoku.grid_span ** 2);
+
+/**
+ * @param {KeyboardEvent} event
+ */
+function onItemKeydown(event) {
+    if (event.key !== "Backspace" && event.key !== "Delete" && event.key.length !== 1) {
+        return false;
+    }
+
+    const value = ["Backspace", "Delete"].includes(event.key) ? 0 : parseInt(event.key);
+
+    if (isNaN(value) || value < 0 || value > 9) {
+        addTransientClass(event.target, "sudoku-item-error", 200);
+    } else if (sudoku.play(event.target.index, value) != PlayResult.Ok) {
+        addTransientClass(event.target, "sudoku-item-warn", 200);
+    } else {
+        event.currentTarget.value = value !== 0 ? value : 0;
+    }
+
+    if (sudoku.isSolved()) {
+        // TODO Win
+    }
+
+    event.preventDefault();
+    return true;
+}
+
+/**
+ *
+ * @param {boolean} initMode
+ */
+function draw() {
+    const grid = document.getElementById("sudoku-grid");
+    grid.innerHTML = "";
+
+    for (const [index, value] of gridBuffer.entries()) {
+        const item = document.createElement("input");
+        item.classList.add("sudoku-item");
+        item.index = index;
+
+        if (value) item.value = value;
+
+        if (!sudoku.isMutableCell(index)) {
+            item.readOnly = true;
+            item.tabIndex = -1;
+        }
+
+        item.addEventListener("keydown", onItemKeydown);
+        grid.appendChild(item);
+    }
+}
+
+function onGenerate() {
+    generateSeed();
+}
+
+/**
+ * @param {MouseEvent} event
+ */
+function onSolve(event) {
+    const result = sudoku.solve();
+
+    if (result === SolveResult.Solved) {
+        draw();
+    } else {
+        addTransientClass(event.currentTarget, "button-shake", 600);
+    }
+}
+
+function onReset() {
+    sudoku.reset();
+    draw();
+}
+
+const generateButton = document.getElementById("generate-button");
+generateButton.addEventListener("click", onGenerate);
+generateButton.classList.remove("hidden");
+
+const solveButton = document.getElementById("solve-button");
+solveButton.addEventListener("click", onSolve);
+solveButton.classList.remove("hidden");
+
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", onReset);
+resetButton.classList.remove("hidden");
+
+const grid = document.getElementById("sudoku-grid");
+grid.style.gridTemplateColumns = `repeat(${sudoku.grid_span}, 1fr)`;
+grid.style.gridTemplateRows = grid.style.gridTemplateColumns;
+
+draw();
--- src/sudoku.rs
+++ src/sudoku.rs
@@ -1,137 +1,224 @@
 use std::{
-    collections::{BTreeSet, HashSet},
-    iter::empty,
+    collections::{HashMap, HashSet},
+    iter::{empty, FromIterator},
+    num::NonZeroU8,
 };
 
-use either::Either;
-use rand::{thread_rng, Rng};
+use rand::{prelude::SliceRandom, rngs::StdRng, Rng, SeedableRng};
 use wasm_bindgen::prelude::*;
-use Either::{Left, Right};
 
-pub const GRID_SIZE: usize = 9;
-pub const BLOCK_SIZE: usize = 3;
+const STD_GRID_SPAN: usize = 9;
+const STD_BLOCK_SPAN: usize = 3;
+const STD_GRID_AREA: usize = STD_GRID_SPAN * STD_GRID_SPAN;
 
-const GRID_AREA: usize = GRID_SIZE * GRID_SIZE;
-const CLUE_QTY: usize = 21;
+#[wasm_bindgen]
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum SolveResult {
+    Solved,
+    Unsolvable,
+}
+
+#[wasm_bindgen]
+#[derive(PartialEq, Debug)]
+pub enum PlayResult {
+    Ok,
+    InvalidMove,
+    InvalidInput,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum SolveStrategy {
+    Random,
+    Ordered,
+}
 
 #[wasm_bindgen]
 pub struct Sudoku {
-    grid: [u8; GRID_AREA],
+    pub grid_span: usize,
+    pub block_span: usize,
+    grid: Vec<Option<NonZeroU8>>,
+    initial_cells: HashSet<usize>,
+    rng: StdRng,
 }
 
 #[wasm_bindgen]
 impl Sudoku {
-    fn get_row(&self, y: usize) -> impl Iterator<Item = u8> + '_ {
-        self.grid[GRID_SIZE * y..GRID_SIZE * (y + 1)]
+    fn get_row_entries(&self, i: usize) -> HashMap<usize, NonZeroU8> {
+        let start = i - i % self.grid_span;
+
+        self.grid[start..start + self.grid_span]
             .iter()
-            .copied()
-    }
-
-    fn get_column(&self, x: usize) -> impl Iterator<Item = u8> + '_ {
-        (0..GRID_AREA)
-            .step_by(GRID_SIZE)
-            .map(move |i| self.grid[i + x])
-    }
-
-    fn get_block(&self, x: usize, y: usize) -> impl Iterator<Item = u8> + '_ {
-        let x0 = x - x % BLOCK_SIZE;
-        let y0 = y - y % BLOCK_SIZE;
-
-        (0..BLOCK_SIZE)
-            .map(move |row| (y0 + row) * GRID_SIZE + x0)
-            .flat_map(move |ys| &self.grid[ys..ys + BLOCK_SIZE])
-            .copied()
-    }
-
-    fn get_intersections(&self, x: usize, y: usize) -> HashSet<u8> {
-        empty()
-            .chain(self.get_block(x, y))
-            .chain(self.get_row(y))
-            .chain(self.get_column(x))
+            .enumerate()
+            .filter_map(|(i, v)| v.map(|v| (i + start, v)))
             .collect()
     }
 
-    fn solve_internal(&mut self, i: usize, random_order: bool) -> bool {
+    fn get_column_entries(&self, i: usize) -> HashMap<usize, NonZeroU8> {
+        let start = i % self.grid_span;
+
+        (start..self.grid_span * self.grid_span)
+            .step_by(self.grid_span)
+            .filter_map(|i| self.grid[i].map(|v| (i, v)))
+            .collect()
+    }
+
+    fn get_block_entries(&self, i: usize) -> HashMap<usize, NonZeroU8> {
+        let x = i % self.grid_span;
+        let y = i / self.grid_span;
+        let x0 = x - x % self.block_span;
+        let y0 = y - y % self.block_span;
+
+        let starts = (0..self.block_span).map(move |row| (y0 + row) * self.grid_span + x0);
+        let indexes = starts.flat_map(|i| i..i + self.block_span);
+
+        indexes
+            .filter_map(|i| self.grid[i].map(|v| (i, v)))
+            .collect()
+    }
+
+    fn get_intersecting_values(&self, i: usize) -> HashSet<NonZeroU8> {
+        empty()
+            .chain(self.get_row_entries(i).into_values())
+            .chain(self.get_column_entries(i).into_values())
+            .chain(self.get_block_entries(i).into_values())
+            .collect()
+    }
+
+    fn get_possible_values(&self, i: usize) -> Vec<NonZeroU8> {
+        let intersectors = self.get_intersecting_values(i);
+
+        (1..=9)
+            .filter_map(NonZeroU8::new)
+            .filter(move |v| !intersectors.contains(v))
+            .collect()
+    }
+
+    fn fill(&mut self, i: usize, strategy: SolveStrategy) -> SolveResult {
         if i >= self.grid.len() {
-            return true;
+            return SolveResult::Solved;
         }
 
-        if self.grid[i] != 0 {
-            return self.solve_internal(i + 1, random_order);
+        if self.grid[i].is_some() {
+            return self.fill(i + 1, strategy);
         }
 
-        let intersections = self.get_intersections(i % GRID_SIZE, i / GRID_SIZE);
+        let mut possible_values = self.get_possible_values(i);
 
-        let candidates = (1..=9).collect::<HashSet<u8>>();
-        let candidates = candidates.difference(&intersections);
+        if strategy == SolveStrategy::Random {
+            possible_values.shuffle(&mut self.rng);
+        }
 
-        let candidates = if random_order {
-            Left(candidates.collect::<HashSet<_>>().into_iter())
-        } else {
-            Right(candidates.collect::<BTreeSet<_>>().into_iter())
-        };
+        for c in possible_values {
+            self.grid[i] = Some(c);
 
-        for &c in candidates {
-            self.grid[i] = c;
-
-            if self.solve_internal(i + 1, random_order) {
-                return true;
+            if self.fill(i + 1, strategy) == SolveResult::Solved {
+                return SolveResult::Solved;
             }
         }
 
-        self.grid[i] = 0;
+        self.grid[i] = None;
 
-        false
+        SolveResult::Unsolvable
     }
 
-    pub fn solve(&mut self) -> bool {
-        self.solve_internal(0, false)
+    pub fn solve(&mut self) -> SolveResult {
+        self.fill(0, SolveStrategy::Ordered)
     }
 
-    pub fn play(&mut self, x: usize, y: usize, value: u8) -> bool {
-        if x >= GRID_SIZE
-            || y >= GRID_SIZE
-            || value < 1
-            || value > 9
-            || self.get_intersections(x, y).contains(&value)
-        {
-            false
-        } else {
-            self.grid[x + y * GRID_SIZE] = value;
-            true
+    fn is_valid_move(&self, i: usize, value: u8) -> bool {
+        if self.initial_cells.contains(&i) {
+            return false;
         }
+
+        if value == 0 {
+            return true;
+        }
+
+        !self
+            .get_intersecting_values(i)
+            .iter()
+            .any(|entry| entry.get() == value)
     }
 
-    pub fn random() -> Self {
-        let mut rng = thread_rng();
-        let mut empty_cells = HashSet::<usize>::with_capacity(GRID_AREA - CLUE_QTY);
-        let mut sudoku = Sudoku {
-            grid: [0; GRID_AREA],
+    pub fn play(&mut self, i: usize, value: u8) -> PlayResult {
+        if i >= self.grid_span * self.grid_span || value > 9 {
+            return PlayResult::InvalidInput;
+        }
+
+        if !self.is_valid_move(i, value) {
+            return PlayResult::InvalidMove;
+        }
+
+        self.grid[i] = match value {
+            0 => None,
+            _ => NonZeroU8::new(value),
         };
 
-        sudoku.solve_internal(0, true);
+        PlayResult::Ok
+    }
 
-        while empty_cells.len() < GRID_AREA - CLUE_QTY {
-            empty_cells.insert((rng.gen::<f32>() * GRID_AREA as f32) as usize);
+    pub fn reset(&mut self) {
+        let deletables = HashSet::from_iter(0..self.grid.len());
+        let deletables = deletables.difference(&self.initial_cells);
+
+        for &i in deletables {
+            self.grid[i] = None;
+        }
+    }
+
+    pub fn random(seed: u64, clues: usize) -> Self {
+        if clues > STD_GRID_AREA {
+            panic!("Number of clues is higher than the grid size.")
         }
 
-        for i in empty_cells {
-            sudoku.grid[i] = 0;
+        let mut sudoku = Sudoku {
+            grid_span: STD_GRID_SPAN,
+            block_span: STD_BLOCK_SPAN,
+            grid: vec![None; STD_GRID_AREA],
+            initial_cells: HashSet::with_capacity(clues),
+            rng: StdRng::seed_from_u64(seed),
+        };
+
+        while sudoku.fill(0, SolveStrategy::Random) != SolveResult::Solved {
+            sudoku.reset();
         }
+
+        while sudoku.initial_cells.len() < clues {
+            sudoku
+                .initial_cells
+                .insert((sudoku.rng.gen::<f32>() * (sudoku.grid_span as f32).powi(2)) as usize);
+        }
+
+        sudoku.reset();
 
         sudoku
+    }
+
+    #[wasm_bindgen(js_name = getGrid)]
+    pub fn get_grid(&self) -> *const Option<NonZeroU8> {
+        self.grid.as_ptr()
+    }
+
+    #[wasm_bindgen(js_name = isMutableCell)]
+    pub fn is_mutable_cell(&self, i: usize) -> bool {
+        !self.initial_cells.contains(&i)
+    }
+
+    #[wasm_bindgen(js_name = isSolved)]
+    pub fn is_solved(&self) -> bool {
+        !self.grid.iter().any(|v| v.is_none())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
+    use std::num::NonZeroU8;
 
-    use crate::sudoku::CLUE_QTY;
+    use rand::{prelude::StdRng, SeedableRng};
 
-    use super::{Sudoku, GRID_AREA, GRID_SIZE};
+    use super::*;
 
-    const TEST_GRID: [u8; GRID_AREA] = [
+    const TEST_VALUES: [u8; 81] = [
         0, 0, 0, 0, 0, 0, 0, 0, 7, // 0
         7, 6, 3, 0, 0, 0, 9, 0, 1, // 1
         1, 0, 5, 3, 6, 7, 2, 4, 8, // 2
@@ -143,120 +230,212 @@ mod tests {
         0, 1, 7, 6, 0, 0, 8, 0, 0, // 8
     ];
 
+    const IMPOSSIBLE_VALUES: [u8; 81] = [
+        2, 5, 6, 0, 4, 9, 8, 3, 7, // 0
+        1, 8, 3, 5, 0, 7, 9, 6, 4, // 1
+        9, 7, 4, 3, 8, 6, 2, 5, 1, // 2
+        8, 4, 9, 1, 6, 2, 3, 7, 5, // 3
+        5, 6, 2, 7, 9, 3, 4, 1, 8, // 4
+        7, 3, 1, 4, 5, 8, 6, 2, 9, // 5
+        6, 9, 7, 8, 3, 1, 5, 4, 2, // 6
+        4, 2, 8, 6, 7, 5, 1, 9, 3, // 7
+        3, 1, 5, 9, 2, 4, 7, 8, 6, // 8
+    ];
+
+    fn create_sudoku(values: [u8; 81]) -> Sudoku {
+        let grid = values
+            .iter()
+            .map(|&v| (NonZeroU8::new(v)))
+            .collect::<Vec<_>>();
+
+        let initial_cells = values
+            .iter()
+            .enumerate()
+            .filter(|(_, &v)| v != 0)
+            .map(|(i, _)| i)
+            .collect();
+
+        Sudoku {
+            grid_span: 9,
+            block_span: 3,
+            rng: StdRng::seed_from_u64(42),
+            grid,
+            initial_cells,
+        }
+    }
+
+    macro_rules! nonzerou8_map {
+        ($( $i:expr => $v:expr ), *) => {
+            std::collections::HashMap::from([
+                    $(
+                        ($i, std::num::NonZeroU8::new($v).unwrap()),
+                    )*
+                ]
+            )
+        };
+    }
+
+    macro_rules! nonzerou8_set {
+        ($( $v:expr ), *) => {
+            std::collections::HashSet::from([
+                    $( std::num::NonZeroU8::new($v).unwrap(), )*
+                ]
+            )
+        };
+    }
+
+    macro_rules! nonzerou8_vec {
+        ($( $v:expr ), *) => {
+            std::vec::Vec::from([
+                    $( std::num::NonZeroU8::new($v).unwrap(), )*
+                ]
+            )
+        };
+    }
+
     #[test]
-    fn test_get_row() {
-        let sudoku = Sudoku { grid: TEST_GRID };
+    fn test_get_row_entries() {
+        let sudoku = create_sudoku(TEST_VALUES);
 
+        assert_eq!(sudoku.get_row_entries(0), nonzerou8_map!(8 => 7));
         assert_eq!(
-            sudoku.get_row(0).collect::<Vec<_>>(),
-            [0, 0, 0, 0, 0, 0, 0, 0, 7]
+            sudoku.get_row_entries(40),
+            nonzerou8_map!(36 => 9, 39 => 1, 40 => 7, 41 => 6, 43 => 8, 44 => 3)
         );
-
         assert_eq!(
-            sudoku.get_row(2).collect::<Vec<_>>(),
-            [1, 0, 5, 3, 6, 7, 2, 4, 8]
-        );
-
-        assert_eq!(
-            sudoku.get_row(GRID_SIZE - 1).collect::<Vec<_>>(),
-            [0, 1, 7, 6, 0, 0, 8, 0, 0]
+            sudoku.get_row_entries(80),
+            nonzerou8_map!(73 => 1, 74 => 7, 75 => 6, 78 => 8)
         );
     }
 
     #[test]
-    fn test_get_column() {
-        let sudoku = Sudoku { grid: TEST_GRID };
+    fn test_get_column_entries() {
+        let sudoku = create_sudoku(TEST_VALUES);
 
         assert_eq!(
-            sudoku.get_column(0).collect::<Vec<_>>(),
-            vec![0, 7, 1, 0, 9, 3, 0, 5, 0]
+            sudoku.get_column_entries(0),
+            nonzerou8_map!(9 => 7, 18 => 1, 36 => 9, 45 => 3, 63 => 5)
         );
-
         assert_eq!(
-            sudoku.get_column(2).collect::<Vec<_>>(),
-            vec![0, 3, 5, 1, 0, 0, 9, 0, 7]
+            sudoku.get_column_entries(40),
+            nonzerou8_map!(22 => 6, 31 => 3, 40 => 7, 67 => 8)
         );
-
         assert_eq!(
-            sudoku.get_column(GRID_SIZE - 1).collect::<Vec<_>>(),
-            vec![7, 1, 8, 0, 3, 0, 5, 9, 0]
+            sudoku.get_column_entries(80),
+            nonzerou8_map!(8 => 7, 17 => 1, 26 => 8, 44 => 3, 62 => 5, 71 => 9)
         );
     }
 
     #[test]
-    fn test_get_block() {
-        let sudoku = Sudoku { grid: TEST_GRID };
+    fn test_get_block_entries() {
+        let sudoku = create_sudoku(TEST_VALUES);
 
         assert_eq!(
-            sudoku.get_block(0, 0).collect::<Vec<_>>(),
-            vec![0, 0, 0, 7, 6, 3, 1, 0, 5]
-        );
-
-        assert_eq!(
-            sudoku.get_block(3, 0).collect::<Vec<_>>(),
-            vec![0, 0, 0, 0, 0, 0, 3, 6, 7]
-        );
-
-        assert_eq!(
-            sudoku.get_block(4, 4).collect::<Vec<_>>(),
-            vec![8, 3, 0, 1, 7, 6, 0, 0, 4]
-        );
-
-        assert_eq!(
-            sudoku
-                .get_block(GRID_SIZE - 1, GRID_SIZE - 1)
-                .collect::<Vec<_>>(),
-            vec![0, 2, 5, 0, 1, 9, 8, 0, 0]
+            sudoku.get_block_entries(40),
+            nonzerou8_map!(30 => 8, 31 => 3, 39 => 1, 40 => 7, 41 => 6, 50 => 4)
         );
     }
 
     #[test]
-    fn test_get_intersections() {
-        let sudoku = Sudoku { grid: TEST_GRID };
+    fn test_get_intersecting_values() {
+        let sudoku = create_sudoku(TEST_VALUES);
 
         assert_eq!(
-            sudoku.get_intersections(0, 0),
-            HashSet::from([0, 1, 3, 5, 6, 7, 9])
+            sudoku.get_intersecting_values(0),
+            nonzerou8_set!(1, 3, 5, 6, 7, 9)
         );
-
         assert_eq!(
-            sudoku.get_intersections(4, 4),
-            HashSet::from([0, 1, 3, 4, 6, 7, 8, 9])
+            sudoku.get_intersecting_values(40),
+            nonzerou8_set!(1, 3, 4, 6, 7, 8, 9)
         );
-
         assert_eq!(
-            sudoku.get_intersections(GRID_SIZE - 1, GRID_SIZE - 1),
-            HashSet::from([0, 1, 2, 3, 5, 6, 7, 8, 9])
+            sudoku.get_intersecting_values(80),
+            nonzerou8_set!(1, 2, 3, 5, 6, 7, 8, 9)
         );
     }
 
     #[test]
-    fn test_play() {
-        let mut sudoku = Sudoku { grid: TEST_GRID };
-
-        assert!(sudoku.play(0, 0, 4));
-        assert!(sudoku.play(GRID_SIZE - 1, GRID_SIZE - 1, 4));
-
-        assert!(!sudoku.play(0, 0, 6));
-        assert!(!sudoku.play(GRID_SIZE - 1, GRID_SIZE - 1, 3));
-        assert!(!sudoku.play(GRID_SIZE - 1, GRID_SIZE - 1, 6));
-        assert!(!sudoku.play(0, 0, 0));
-        assert!(!sudoku.play(0, 0, 10));
-        assert!(!sudoku.play(10, 0, 4));
-        assert!(!sudoku.play(0, 10, 0));
+    fn test_get_possible_values() {
+        let sudoku = create_sudoku(TEST_VALUES);
+        assert_eq!(sudoku.get_possible_values(0), nonzerou8_vec!(2, 4, 8));
+        assert_eq!(sudoku.get_possible_values(40), nonzerou8_vec!(2, 5));
+        assert_eq!(sudoku.get_possible_values(80), nonzerou8_vec!(4));
     }
 
     #[test]
     fn test_solve() {
-        let mut sudoku = Sudoku { grid: TEST_GRID };
-        assert!(sudoku.solve());
-        assert!(sudoku.grid.iter().all(|&v| v != 0));
+        let mut sudoku = create_sudoku(TEST_VALUES);
+        assert_eq!(sudoku.solve(), SolveResult::Solved);
+
+        let mut impossible = create_sudoku(IMPOSSIBLE_VALUES);
+        assert_eq!(impossible.solve(), SolveResult::Unsolvable);
+    }
+
+    #[test]
+    fn test_is_valid_move() {
+        let sudoku = create_sudoku(TEST_VALUES);
+
+        assert!(sudoku.is_valid_move(0, 0));
+        assert!(!sudoku.is_valid_move(9, 0));
+        assert!(!sudoku.is_valid_move(40, 4));
+        assert!(!sudoku.is_valid_move(80, 6));
+        assert!(!sudoku.is_valid_move(80, 3));
+    }
+
+    #[test]
+    fn test_play() {
+        let mut sudoku = create_sudoku(TEST_VALUES);
+
+        assert_eq!(sudoku.play(0, 10), PlayResult::InvalidInput);
+        assert_eq!(sudoku.play(100, 0), PlayResult::InvalidInput);
+        assert_eq!(sudoku.play(0, 7), PlayResult::InvalidMove);
+        assert_eq!(sudoku.play(0, 4), PlayResult::Ok);
+        assert_eq!(sudoku.grid[0].unwrap().get(), 4);
+        assert_eq!(sudoku.play(0, 0), PlayResult::Ok);
+        assert_eq!(sudoku.grid[0], None);
+    }
+
+    #[test]
+    fn test_reset() {
+        let mut sudoku = create_sudoku(TEST_VALUES);
+        sudoku.grid[0] = NonZeroU8::new(4);
+        sudoku.grid[80] = NonZeroU8::new(4);
+        sudoku.reset();
+
+        assert_eq!(sudoku.grid[0], None);
+        assert_eq!(sudoku.grid[80], None);
     }
 
     #[test]
     fn test_random() {
-        let mut sudoku = Sudoku::random();
-        assert_eq!(sudoku.grid.iter().filter(|&&v| v != 0).count(), CLUE_QTY);
-        assert!(sudoku.solve());
+        let mut sudoku0 = Sudoku::random(42, 10);
+        let sudoku1 = Sudoku::random(42, 10);
+        let sudoku2 = Sudoku::random(100, 10);
+
+        assert_eq!(sudoku0.grid, sudoku1.grid);
+        assert_ne!(sudoku0.grid, sudoku2.grid);
+        assert_eq!(sudoku0.solve(), SolveResult::Solved);
+        assert_eq!(sudoku0.initial_cells.len(), 10);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_random_clues() {
+        Sudoku::random(42, 100);
+    }
+
+    #[test]
+    fn test_is_mutable_cell() {
+        let sudoku = create_sudoku(TEST_VALUES);
+        assert!(sudoku.is_mutable_cell(0));
+        assert!(!sudoku.is_mutable_cell(40));
+    }
+
+    #[test]
+    fn test_is_solved() {
+        let mut sudoku = create_sudoku(TEST_VALUES);
+        assert!(!sudoku.is_solved());
+        sudoku.solve();
+        assert!(sudoku.is_solved());
     }
 }
